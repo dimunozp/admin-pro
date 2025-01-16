13de673c106c87bdf1e54f0289e70c0bf381e576
dimunozp <dimamuno@espol.edu.ec>
2025-01-16 00:02:56 -0500

    feat(008): implementing progress page

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-progress',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './progress.component.html',
-  styles: ``
+  styleUrls: ['./progress.component.css']
 })
 export class ProgressComponent {
+
+  progress: number = 50;
+
+  get getPercentage() {
+    return `${this.progress}%`;
+  }
+
+  changePercentage(value: number): number {
+
+    if(this.progress >= 100 && value >= 0)
+      return this.progress = 100;
+
+    if(this.progress <= 0 && value < 0)
+      return this.progress = 0;
+
+    return this.progress += value;
+  }
 
 }
